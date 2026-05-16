bc82e5e1061d599d5a4d99a5be47c6a6d7e9e847
Alyvia-Vaughan <alvaughan@ucsd.edu>
2026-05-15 20:58:54 -0700

    commit -m "finish lab 7 step 3.1"

--- map.js
+++ map.js
@@ -1,3 +1,4 @@
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 console.log('Mapbox GL JS Loaded:', mapboxgl);
 
@@ -46,5 +47,19 @@ map.on('load', async () => {
         'line-opacity': 0.7,
     },
     });
+
+    let jsonData;
+    try {
+        const jsonurl = 'https://dsc106.com/labs/lab07/data/bluebikes-stations.json';
+
+        // Await JSON fetch
+        jsonData = await d3.json(jsonurl);
+
+        console.log('Loaded JSON Data:', jsonData); // Log to verify structure
+    } catch (error) {
+        console.error('Error loading JSON:', error); // Handle errors
+    }
+    let stations = jsonData.data.stations;
+    console.log('Stations Array:', stations);
 });
 
